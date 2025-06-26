ea511c10dde1ca18309b30e8d437c14230a1f5e1
froas <froas.crp@gmail.com>
2025-06-26 16:23:45 +0900

    Fix header

--- src/components/NewParticles.tsx
+++ src/components/NewParticles.tsx
@@ -90,7 +90,7 @@ const fragment = /* glsl */ `
 `;
 
 const Particles: React.FC<ParticlesProps> = ({
-  particleCount = 200,
+  particleCount = 1000,
   particleSpread = 10,
   speed = 0.1,
   particleColors,
